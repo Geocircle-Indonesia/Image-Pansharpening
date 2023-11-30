ff16b98dce82e20e3468ca2b5059f8afe5a7fb7a
SANDI <sandi@SANDIs-MacBook-Pro.local>
2023-11-30 21:02:48 +0700

    repair script and add export to geotif 8bit

--- create-sample.py
+++ create-sample.py
@@ -1,41 +1,78 @@
 import rasterio
 import numpy as np
+import uuid
 from rasterio.transform import from_origin
 from rasterio.enums import Resampling
-from rasterio.windows import Window
+from PIL import Image
+from skimage import exposure
 
 # Create a function to read 4 bands and 1 bands image. Store as numpy array
 def read_images(multispectral, panchromatic):
-    with rasterio.open(multispectral) as src:
-        multispectral_image = src.read()
-
-    with rasterio.open(panchromatic) as src:
-        panchromatic_image = src.read()  
+    
+    with rasterio.open(panchromatic) as src_p:
+        panchromatic_image = src_p.read()
+        panchromatic_image = np.transpose(panchromatic_image, (1, 2, 0))
+    
+    with rasterio.open(multispectral) as src_ms:
+        #multispectral_image = src_ms.read()
+        multispectral_image = src_ms.read(
+            out_shape=(src_ms.count, src_p.height, src_p.width),
+            resampling=Resampling.nearest
+        )
+        #multispectral_image = multispectral_image.astype(np.uint8)
+        multispectral_image = np.transpose(multispectral_image, (1, 2, 0))
 
     return multispectral_image, panchromatic_image
 
+def sliding_window(image, window_size):
+    height, width = image.shape[:2]
+    window_width, window_height = window_size
 
-# Create a function to cut the array into particular size and save as tif image using looping and sliding windows technique
-def save_tif_from_array(array, output_path, window_size):
-    height, width = array.shape[1:]
+    for y in range(0, height - window_height + 1, window_height):
+        for x in range(0, width - window_width + 1, window_width):
+            window = image[y:y + window_height, x:x + window_width]
+            yield window, (x, y, x + window_width, y + window_height)
 
-    for i in range(0, height, window_size):
-        for j in range(0, width, window_size):
-            window = Window(j, i, min(window_size, width - j), min(window_size, height - i))
+def generate_random_key_value():
+    # Generate a random key using UUID
+    random_key = str(uuid.uuid4())
+    return random_key
 
-            windowed_array = array[:, window.row_off:window.row_off + window.height, window.col_off:window.col_off + window.width]
+def export_array_to_image(array, output_path):
 
-            transform = from_origin(window.col_off, window.row_off, window_size, window_size)
+    array = exposure.rescale_intensity(array, out_range='uint8')
+    # Get the dimensions of the array
+    height, width, bands = array.shape
+    projection = 'EPSG:4326'
 
-            with rasterio.open(output_path, 'w', driver='GTiff', height=window.height, width=window.width, count=array.shape[0], dtype=array.dtype, crs='+proj=latlong', transform=transform) as dst:
-                dst.write(windowed_array)
+    # Create a GeoTIFF file
+    with rasterio.open(
+        output_path,
+        'w',
+        driver='GTiff',
+        height=height,
+        width=width,
+        count=bands,
+        dtype=array.dtype,
+        crs=projection,
+    ) as dst:
+        # Write the array data to the GeoTIFF bands
+        for band in range(bands):
+            dst.write(array[:, :, band], band + 1)
 
 
-multispectral_path = "IMG_PHR1A_MS_202310170327506_ORT_265b5d70-f3ff-4f64-cf82-62111555cf84-001_R1C1.TIF"
-panchromatic_path = "IMG_PHR1A_P_202310170327506_ORT_265b5d70-f3ff-4f64-cf82-62111555cf84-002_R1C1.TIF"
+if __name__ == "__main__":
+    multispectral_path = "./SPOT_RENDER/MS4_1/MS4_1.tif"
+    panchromatic_path = "./SPOT_RENDER/P_1.tif"
+    window_size = (1000,1000)
 
-multispectral_array, panchromatic_array = read_images(multispectral_path, panchromatic_path)
-
-window_size = 256
-save_tif_from_array(multispectral_array, "output_multispectral.tif", window_size)
-save_tif_from_array(panchromatic_array, "output_panchromatic.tif", window_size)
+    multispectral_array, panchromatic_array = read_images(multispectral_path, panchromatic_path)
+    for _, coordinates in sliding_window(panchromatic_array, window_size):
+        start_x, start_y, end_x, end_y = coordinates
+        cropped_ms = multispectral_array[start_y:end_y, start_x:end_x, :3]
+        cropped_p = panchromatic_array[start_y:end_y, start_x:end_x,:]
+       # print(cropped_ms.shape, type(cropped_p))
+        key = generate_random_key_value()
+        export_array_to_image(cropped_ms, f'./DATA/MS/MS_{key}.tif')
+        export_array_to_image(cropped_p, f'./DATA/P/P_{key}.tif')
+        print(key)
